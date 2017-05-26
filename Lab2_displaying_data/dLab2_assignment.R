25fa2b38259cd7a5990434c59fde383b1eadfe79
brouwern <brouwern@gmail.com>
2017-05-26 12:23:02 -0400

    lab 2 uploaded

---
+++ Lab2_displaying_data/dLab2_assignment.R
@@ -0,0 +1,26 @@
+
+
+
+
+
+### In-class assignment
+
+#Enter your own state
+#What is min, max, mean, median
+#sd
+#make a boxplot - what does it representatio
+#what is bigger, sd or se?
+
+
+### Lab Write-up 1:
+
+# 1)Find recent (2014,2015 or 2016) data on the
+# number of eagles nesting in your state
+# 2)Add the data to the excel file
+# 3)Make a plot that includes PA, OH, and your state
+# 4)Include a legend using the legend() command
+# 5)Export the graph
+# 6)Write a short legend (aka caption) beneath the graph that
+# explains what it is like it woudl show 
+# up in a published paper
+# An example will be provided in class
